4543d3dcc0ba554f451f3cc8874551efcd59efcd
laxmis-prog <laxmisanuu5@gmail.com>
2025-12-05 18:45:16 +0200

    Update entryNumber variable to be the value of the length of the query plus 1,to fix the entry counts in breakfast category.

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ function addEntry() {
   const targetInputContainer = document.querySelector(
     `#${entryDropdown.value} .input-container`
   );
-  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length;
+  const entryNumber = targetInputContainer.querySelectorAll('input[type="text"]').length+1;
   const HTMLString = `<label for = "${entryDropdown.value}-${entryNumber}-name">Entry ${entryNumber} Name </label>
    <input type="text" id="${entryDropdown.value}-${entryNumber}-name" placeholder="Name" />
     <input type="number" min="0" placeholder="Calories" id="${entryDropdown.value}-${entryNumber}-calories">`;
