6e83ab8cd4406a2d75c7f2c4bcc98923b2ff5c72
laxmis-prog <laxmisanuu5@gmail.com>
2025-12-15 15:45:00 +0200

    Declare a breakfastNumberInputs variable.

--- script.js
+++ script.js
@@ -32,6 +32,8 @@ function addEntry() {
 function calculateCalories(e) {
   e.preventDefault();
   isError = false;
+  const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
+
 }
 
 function getCaloriesFromInputs(list) {
