38be3b6c77d570f81c08bdbfb2fcc694c7aa2898
laxmis-prog <laxmisanuu5@gmail.com>
2025-12-15 15:49:06 +0200

    Using same syntax,query number inputs in the #lunch element and assign them to lunchNumberInputs.

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ function calculateCalories(e) {
   e.preventDefault();
   isError = false;
   const breakfastNumberInputs = document.querySelectorAll("#breakfast input[type='number']");
-
+  const lunchNumberInputs = document.querySelectorAll("#lunch input[type='number']");
 }
 
 function getCaloriesFromInputs(list) {
